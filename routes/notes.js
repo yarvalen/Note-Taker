8f8e79c2f47ca26258df5516ef4a87c2950995f6
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-04-11 17:36:16 -0400

    updated notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,37 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
     readFromFile,
     readAndAppend,
     writeToFile,
   } = require('../helpers/fsUtils');
 
+  const createdId = require('createdId');
+
 router.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+// POST route 
+notes.post('/', (req, res) => {
+    
+    const { title, text } = req.body;
+   // information for title and text is added
+    if (title && text) {
+        const newNote = {
+            title, 
+            text, 
+            id: createdId({length: 6})
+        }
+        console.log(title, text)
+        readAndAppend(newNote, './db/db.json')
+        res.json('Note added succesfully')
+    } else {
+        res.json('Error in adding the note')
+    }
+})
+  
+
+
+
+
 module.exports = router
