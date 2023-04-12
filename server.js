767d1b6e9246a847ca456d6228b47117514df2d1
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-04-11 21:41:10 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
